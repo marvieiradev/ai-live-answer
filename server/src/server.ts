33d1e082036b06425c3992d575d88beeb9b5045c
marvieiradev <marcosvieira482@gmail.com>
2025-07-10 20:37:11 -0300

    feat: implement room creation and question retrieval endpoints

--- server/src/server.ts
+++ server/src/server.ts
@@ -6,8 +6,9 @@ import {
 } from "fastify-type-provider-zod";
 import { fastifyCors } from "@fastify/cors";
 import { env } from "./env.ts";
-import { sql } from "./db/connection.ts";
 import { getRoomsRoute } from "./http/routes/get-rooms.ts";
+import { createRoomRoute } from "./http/routes/create-room.ts";
+import { getRoomQuestions } from "./http/routes/get-room-questions.ts";
 
 const app = fastify().withTypeProvider<ZodTypeProvider>();
 
@@ -23,6 +24,8 @@ app.get("/health", () => {
 });
 
 app.register(getRoomsRoute);
+app.register(createRoomRoute);
+app.register(getRoomQuestions);
 
 app.listen({ port: env.PORT }).then(() => {
   console.log("Server Running!!");
