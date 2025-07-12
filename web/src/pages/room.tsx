61c99c16cdaf0ae98a548f0f1646cc3bde8adb67
marvieiradev <marcosvieira482@gmail.com>
2025-07-12 13:11:58 -0300

    feat: implement create question functionality with API integration and form handling

--- web/src/pages/room.tsx
+++ web/src/pages/room.tsx
@@ -2,7 +2,6 @@ import { ArrowLeft, Radio } from "lucide-react";
 import { Link, Navigate, useParams } from "react-router-dom";
 import { Button } from "@/components/ui/button";
 import { QuestionForm } from "@/components/question-form";
-import { QuestionItem } from "@/components/question-item";
 import { QuestionList } from "@/components/question-list";
 
 type RoomParams = {
